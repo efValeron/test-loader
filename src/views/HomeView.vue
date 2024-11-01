45631873ca64684db5d0ba245ae2a91d494cc5f1
Valeriy <masterbox06@gmail.com>
2024-11-01 15:08:52 +0400

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h1 class="heading">Hello New Vue Project!</h1>
+  </main>
+</template>
+
+<style scoped>
+.heading {
+  color: red;
+  font-size: 50px;
+}
+</style>
